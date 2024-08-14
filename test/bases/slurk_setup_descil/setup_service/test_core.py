7d128072e09a92978b2ad7512ca5c66398b369e4
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2024-08-14 15:02:54 +0200

    finish rename setup_client->setup_service

--- test/bases/slurk_setup_descil/setup_service/test_core.py
+++ test/bases/slurk_setup_descil/setup_service/test_core.py
@@ -4,5 +4,8 @@ from slurk_setup_descil.setup_service.core import (
     setup_waiting_room,
 )
 
+
 def test():
     assert create_waiting_room_tokens is not None
+    assert setup_and_register_concierge is not None
+    assert setup_waiting_room is not None
