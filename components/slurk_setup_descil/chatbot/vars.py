10e0a0f6b710d20445ca1de81e907ebebd75b68c
Uwe Schmitt <uwe.schmitt@id.ethz.ch>
2024-09-20 13:00:52 +0200

    remove vars.py with secrets

--- components/slurk_setup_descil/chatbot/vars.py
+++
@@ -1,4 +0,0 @@
-AZURE_OPENAI_ENDPOINT = "https://css-openai.openai.azure.com/"
-AZURE_OPENAI_API_KEY = "***REMOVED***"
-AZURE_OPENAI_MODEL = "css-openai-gpt35"
-AZURE_OPENAI_API_VERSION = "2023-12-01-preview"
